1002fddd36185ff1bf514878c5340f2d2317bf8b
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-09-17 01:08:25 -0500

    actions incrementar padre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,14 +16,20 @@ export class AppComponent {
   contador = 10;
 
 
+  constructor(private store: Store<AppState>) {
+      this.store.subscribe(state => {
+        this.contador = state.contador
+      });
+
+  }
+
 
 incrementar() {
-
-
+  this.store.dispatch(actions.incrementar());
 }
 
 decrementar() {
-
+ this.store.dispatch(actions.decrementar());
 }
 
 }
