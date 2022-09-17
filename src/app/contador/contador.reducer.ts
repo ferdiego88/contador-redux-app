1620b8c330688d370ef88da61466777b819f0adc
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-09-17 12:56:18 -0500

    create Reducer Ok

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,4 +1,4 @@
-import { Action, createReducer, on } from "@ngrx/store";
+import {createReducer, on } from "@ngrx/store";
 import { decrementar, incrementar } from "./contador.actions";
 
 export const initialState =20;
